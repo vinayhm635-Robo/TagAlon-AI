eefd4385f01ec1faf67cd82397aee1dbdc146288
vinayhm635-Robo <vinayhm635@gmail.com>
2026-08-23 10:39:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function addItems() {
-
     let item = document.getElementById("itemInput").value;
 
-    if (item === "") {
+    if (item.trim() === "") {
         alert("Please enter the items you forgot.");
-    } else {
-
-        localStorage.setItem("forgottenItems", item);
-
-        window.location.href = "success.html";
+        return;
     }
+
+    localStorage.setItem("forgottenItems", item);
+
+    window.location.href = "./success.html";
 }
